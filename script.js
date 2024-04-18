ac38fba647d5de0f3a8386a77945bc3b7e782c23
Demilade <jaydenosholowu@gmail.com>
2024-04-18 10:49:09 +0100

    Modified the portfolio webpage

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".menu-icon");
-
+function toggleMenu() {
+    const menu = document.querySelector(".media-links");
+    const icon = document.querySelector(".media-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-}
+  }
+  
